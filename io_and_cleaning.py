53552839e718c5c3bd07dbf7b9574705e8e5a2f6
Bartosz Brzeziński <bartosz.brzezinski97@gmail.com>
2023-01-27 01:11:27 +0100

    io and cleaning data completed

--- io_and_cleaning.py
+++ io_and_cleaning.py
@@ -6,22 +6,26 @@ def input(gdp_file,pop_file,fos_file):
     pop_data=pd.read_csv(pop_file,header=2)
     fos_data=pd.read_csv(fos_file,header=0)
     return gdp_data,pop_data,fos_data
+
 def timeframe(gdp_data,pop_data,fos_data):
     set_gdp=set(gdp_data.columns)
     set_pop=set(pop_data.columns)
     set_fos=set(str(i) for i in fos_data['Year'])
     return list(set.intersection(set_gdp,set_pop,set_fos))
+
 def join_dataframes(gdp_data,pop_data,fos_data):
     time_interval=timeframe(gdp_data,pop_data,fos_data)
     gdp_melt=gdp_data.melt(id_vars="Country Name",value_vars=time_interval,var_name='Year',value_name='GDP')
     gdp_melt['Year']=gdp_melt['Year'].apply(int)
+    gdp_melt.dropna(subset=['GDP'], inplace=True)
     gdp_melt['Country Name']=gdp_melt['Country Name'].str.upper()
     pop_melt=pop_data.melt(id_vars="Country Name",value_vars=time_interval,var_name='Year',value_name='Population')
     pop_melt['Year']=pop_melt['Year'].apply(int)
+    pop_melt.dropna(subset=['Population'], inplace=True)
     pop_melt['Country Name']=pop_melt['Country Name'].str.upper()
-    fos_data.rename(columns={'Country':'Country Name'}, inplace=True)
-    joined_df = pd.merge(gdp_melt, pop_melt,how='left', left_on=['Country Name','Year'], right_on=['Country Name','Year'])
-    joined_df = pd.merge(joined_df, fos_data, how='inner', left_on=['Country Name','Year'], right_on=['Country Name','Year'])
+    fos_data.rename(columns={'Country':'Country Name','Total':'Total CO2'}, inplace=True)
+    joined_df = pd.merge(gdp_melt, pop_melt,how='inner', left_on=['Country Name','Year'], right_on=['Country Name','Year'])
+    joined_df = pd.merge(joined_df, fos_data[['Country Name','Year','Total CO2']], how='inner', left_on=['Country Name','Year'], right_on=['Country Name','Year'])
     return joined_df
 
 def filtering_data(data,start='nan',finish='nan'):
