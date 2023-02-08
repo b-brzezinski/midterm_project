4222f7f0852ce2e243c4d8ac764da1adc71edad3
Bartosz Brzeziński <bartosz.brzezinski97@gmail.com>
2023-02-08 17:37:25 +0100

    added proper setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='midterm_project',
     version='0.0.1',
+    py_modules=['io_and_cleaning','analysis'],
     install_requires=[
         'jupyterlab',
         'pandas']
